2dd10b8c01e0cf78c84d455e2aff1589b2a8232a
andrewbuko <89219042+andrewbuko@users.noreply.github.com>
2022-02-11 09:12:09 -0600

    fixed
    bam

--- Intro Code/nim.java
+++ Intro Code/nim.java
@@ -5,23 +5,47 @@ import java.util.*;
  * Nim
  * Simulating the game of nim
  */
-public class nim
+public class nimm
 {
     public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
-        int[] nims = {3,5,8};
+        int[] nim = {3,5,8};
         boolean done = false; boolean p1take = false; boolean p2take = false;
         while(!done)
         {
-            System.out.println("Player 1 state which pile you would like to take from: 1 2 or 3");
-            System.out.println("Player 1 state how much you would like to take from that pile");
-            nim.display(nims);
-            int x = scan.nextInt();
             while(!p1take)
             {
-                
-            }    
+              nimm.display(nim);
+              System.out.println("Player 1 state which pile you would like to take from: 1 2 or 3");
+              int x1 = scan.nextInt();
+              System.out.println("Player 1 state how much you would like to take from that pile");
+              int y1 = scan.nextInt();
+              if(x1!=1&&x1!=2&&x1!=3 || nim[x1-1]<y1 || y1 < 1)
+                System.out.println("Please take a valid amount of stones from a valid pile");
+              else
+              {
+                nim[x1 - 1] = nim[x1-1] - y1;
+                p1take = true;
+              }    
+            }  
+            while(!p2take)
+            {
+              nimm.display(nim);
+              System.out.println("Player 2 state which pile you would like to take from: 1 2 or 3");
+              int x2 = scan.nextInt();
+              System.out.println("Player 2 state how much you would like to take from that pile");
+              int y2 = scan.nextInt();
+              if((x2!=1&&x2!=2&&x2!=3) || nim[x2-1]<y2 || y2 < 1)
+                System.out.println("Please take a valid amount of stones from a valid pile");
+              else
+              {
+                nim[x2 - 1] = nim[x2-1] - y2;
+                p2take = true;
+              }    
+            } 
+            p1take = false;
+            p2take = false;   
         }
     }    
     public static void display(int[] x)
